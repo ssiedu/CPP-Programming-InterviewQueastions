b59d61615794977d615298a233191400727c0a75
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-12-03 13:02:33 +0530

    IQ31

---
+++ IQ31.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+class Complex
+{
+	int a,b;
+	public:
+		Complex()
+		{
+			a=10;
+			b=20;
+		}
+		Complex(int x, int y)
+		{
+			a=x;
+			b=y;
+		}
+		Complex(int x)
+		{
+			a=x;
+			b=0;
+		}
+		
+		void printNumber()
+		{
+			cout<<"Your Number is "<<a << " + "<<b<< "i"<<endl;
+		}
+				
+};
+
+int main()
+{
+	Complex c1(4,6);
+	c1.printNumber();
+	
+	Complex c2(5);
+	c2.printNumber();
+	
+	Complex c3;
+	c3.printNumber();
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
